24216fed0984ad66de6328ee4ab66f42de1d3d83
Ana Flávia Cador <aflaviah15@gmail.com>
2020-08-13 19:09:54 -0300

    dev

--- header.php
+++ header.php
@@ -11,3 +11,22 @@ echo '<body class="'.join(' ',get_body_class()).'">';
 
 echo '<main>';
 
+	echo '<header id="cabecalho" class="container">';
+		echo '<div class="nav-mini">';
+			echo '<div>&nbsp;</div>';
+
+			if (class_exists('Woocommerce')) { 
+				echo '<div class="menu-loja">';
+					echo '<a href="#" class="conta">';
+						echo '<i class="fas fa-user-lock"></i><span> Cliente</span>';
+					echo '</a>';
+					echo '<a href="#" class="carrinho">';
+						echo '<i class="fas fa-shopping-cart"></i><span> 1</span>';
+					echo '</a>';
+				echo '</div>';
+			}
+		echo '</div>';
+
+		// echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab temporibus a perferendis aspernatur necessitatibus tempora est accusantium ut voluptatum voluptatibus.</p>';
+	echo '</header>';
+
