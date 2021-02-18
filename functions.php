80e37b8b31ec99a8d47185be3fad53e9dd2d2e6f
Ana Flávia Cador <aflaviah15@gmail.com>
2021-02-18 18:44:29 -0300

    hotjar heatmap

--- functions.php
+++ functions.php
@@ -237,6 +237,9 @@ function afc_load_scripts_head() {
     echo '<script async src="https://www.googletagmanager.com/gtag/js?id=UA-10144283-7"></script>';
     echo '<script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag(\'js\', new Date()); gtag(\'config\', \'UA-10144283-7\');</script>';
 
+    ////////////// HOTJAR - HEAT MAP
+    echo '<script async>(function(h,o,t,j,a,r){ h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}; h._hjSettings={hjid:2255047,hjsv:6}; a=o.getElementsByTagName(\'head\')[0]; r=o.createElement(\'script\');r.async=1; r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv; a.appendChild(r); })(window,document,\'https://static.hotjar.com/c/hotjar-\',\'.js?sv=\');</script>';
+
     ////////////// Facebook Pixel Code
     echo '<script>!function(f,b,e,v,n,t,s) {if(f.fbq)return;n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version=\'2.0\'; n.queue=[];t=b.createElement(e);t.async=!0; t.src=v;s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)}(window, document,\'script\', \'https://connect.facebook.net/en_US/fbevents.js\'); fbq(\'init\', \'1002685960227949\'); fbq(\'track\', \'PageView\');</script><noscript><img height="1" width="1" style="display:none"
       src="https://www.facebook.com/tr?id=1002685960227949&ev=PageView&noscript=1"/></noscript>';
