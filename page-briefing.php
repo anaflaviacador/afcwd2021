d5cb9cd0ee4f7ae27d85fb74c6629553b1b3609c
Ana Flávia Cador <aflaviah15@gmail.com>
2021-07-07 15:23:10 -0300

    novo formulario de briefing

--- page-briefing.php
+++ page-briefing.php
@@ -11,16 +11,35 @@ echo '<article class="container" id="pagina-briefing">';
 	$tembriefing = count_user_posts( $userid , 'af_entry' );
 	$urlsite = get_bloginfo('url');
 
-	if ($tembriefing >= 1){
-		echo '<blockquote style="border:0;background:var(--cor-negacao);color:white; width:100%;font-size:.8em;">';
-		echo '<p class="has-text-align-center">Você já tem briefing preenchido, '.$cliente.'! <a href="'.$urlsite.'/wp-admin/edit.php?s&post_status=publish&post_type=af_entry&author='.$userid.'&action=-1&m=0&entry_form=form_5cc98ff56cee8&filter_action=Filtrar&paged=1&action2=-1" target="_blank" style="color:white;text-decoration:underline"><strong>Clique aqui para editar</strong></a> caso não esteja solicitando mais de um projeto.</p>';
-		echo '</blockquote>';
+	$formADICIONAL = '5785'; // local 5835 
+	$formBRIEFING = '5786'; // local 5827
+
+	// captura entradas do form de briefin
+	$entries = wpforms()->entry->get_entries(['form_id' => '5827']);
+	
+	
+	$clienteEnviouForm = false; // declara 1º como false se enviou form
+	foreach ($entries as $entry) {
+		if ($entry->user_id == $userid) {
+			$clienteEnviouForm = true; // consegue ver se o user enviou o form	
+			break;
+		}
 	}
-		
+
+	if($clienteEnviouForm) {	
+		// colocar aqui a condicional para qdo cliente ja enviou briefing
+			echo '<blockquote style="border:0;background:var(--cor-negacao);color:white; width:100%;font-size:.8em;">';
+			echo '<p class="has-text-align-center"><strong>Você já tem um briefing enviado no sistema, '.$cliente.'!</strong><br>Caso não esteja solicitando um novo projeto, <a href="#briefing-adicional" class="abre-modal" data-target="#briefing-adicional"><strong style="color:white;text-decoration:underline">CLIQUE AQUI</strong></a> e acrescente as informações adicionais ao briefing enviado anteriormente.</p>';
+			echo '</blockquote>';
+
+			echo '<div class="modal" id="briefing-adicional">';
+				echo '<h2 class="has-text-align-center">Informações adicionais</h2>';
+				echo '<article><p>Esqueceu de acrescentar algum detalhe no briefing? Não tem problema! Adicione abaixo as informações adicionais a um formulário que você já enviou. Assim, você não precisará preencher tudo novamente. Será mantido tudo em histórico de email e banco de dados. 😉</p>'.do_shortcode('[wpforms id="'.$formADICIONAL.'"]').'</article>'; 
+			echo '</div>'; 
+	}	
 
 	if (have_posts()) {
 		while (have_posts()) : the_post();
-
 			echo '<article class="sidebar">';
 				echo '<h5 class="cursivo saudacoes">Oi, '.$cliente.'!</h5>';
 				the_content(); 
@@ -28,7 +47,9 @@ echo '<article class="container" id="pagina-briefing">';
 		endwhile;
 	}		
 	
-	echo do_shortcode('[advanced_form form="form_5cc98ff56cee8" ajax="0"]');
+	echo '<div class="form-briefing">';
+		echo do_shortcode('[wpforms id="'.$formBRIEFING.'" title="false"]');
+	echo '</div>';		
 
 echo '</article>';		
 	
