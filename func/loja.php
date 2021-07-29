838e1e5b3072dfc557f2e2bee2d979e1ddf83f86
Ana Flávia Cador <aflaviah15@gmail.com>
2021-07-29 15:23:53 -0300

    redirect e inicio de construcao de loop de produtos

--- func/loja.php
+++ func/loja.php
@@ -100,6 +100,22 @@ function afc_redireciona_qdo_sai(){
     exit;
 }
 
+// redireciona direto ao carrinho quando add to cart
+add_filter('add_to_cart_redirect', 'afc_cart_redirect');
+function afc_cart_redirect() {
+    global $woocommerce;
+    $redirect_checkout = $woocommerce->cart->get_checkout_url();
+    return $redirect_checkout;
+}
+
+
+// ========================================//
+// LOOP DE PRODUTOS
+// ========================================// 
+remove_action('woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10 );
+remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5 );
+
+
 // ========================================//
 // PRODUTO
 // ========================================// 
