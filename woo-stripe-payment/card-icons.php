760d15a1d5ff2085344cc2c03b587d35dea59270
Ana Flávia Cador <aflaviah15@gmail.com>
2021-10-25 11:16:55 -0300

    stripe

--- woo-stripe-payment/card-icons.php
+++ woo-stripe-payment/card-icons.php
@@ -3,4 +3,4 @@
  * @version 3.2.8
  */
 
-// echo '<span class="wc-stripe-card-icons-container" style="float:none;"><img width="95px" src="'.get_stylesheet_directory_uri();.'/img/flag-globe.svg" /></span>';
+echo '<span class="wc-stripe-card-icons-container" style="float:none;"><img width="48px" src="'.get_stylesheet_directory_uri().'/img/logo-stripe.svg" style="bottom:-5px!important" /></span>';
