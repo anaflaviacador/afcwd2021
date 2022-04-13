05d4b79c3c2df45d8b1f64c6e0ed0b9aea22b669
Ana Flávia Cador <aflaviah15@gmail.com>
2022-04-13 13:25:08 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -7,26 +7,35 @@ $urlTema = get_template_directory_uri();
 // ========================================// 
 // if ((! is_post_type_archive('afc_depoimentos') && ! is_singular('private-page') && ! is_singular('etheme_portfolio') && ! is_page('contato') && ! is_page('briefing') && ! is_page('afiliacao') && ! is_page('planos')) && afc_nao_woocommerce()) {
 
+global $product;
+$ativa_LP = get_field('ativar_lp',$product->get_ID());
 
-if (is_front_page() || is_post_type_archive(array('etheme_portfolio')) || is_page(array('servicos','sobre')) || is_singular('afc_blog')) {
+if (is_front_page() || is_post_type_archive(array('etheme_portfolio')) || (is_singular('product') && $ativa_LP == true) || is_page(array('servicos','sobre')) || is_singular('afc_blog')) {
 
 	echo '<section id="depoimentos-incriveis" class="no-rodape">';
 		echo '<div class="container">';
 			echo '<span class="gutter-sizer"></span><span class="grid-sizer"></span>';
 
+
 			echo '<div class="depoimento"><div class="intro">';
 				echo '<h2 class="cursivo assinado">Clientes</h2>';
-				echo '<p><strong>Depoimentos</strong> de clientes que tiraram seus projetos do papel e permitiram que o studio <span class="afc">'; echo get_template_part('img/afc'); echo '</span> os tornassem <strong>reais</strong>!</p>';
+				if (is_singular('product') && $ativa_LP == true) {
+					echo '<p><strong>Depoimentos</strong> de quem comprou um tema da loja <span class="afc">'; echo get_template_part('img/afc'); echo '</span></p>';
+				} else {
+					echo '<p><strong>Depoimentos</strong> de clientes que tiraram seus projetos do papel e permitiram que o studio <span class="afc">'; echo get_template_part('img/afc'); echo '</span> os tornassem <strong>reais</strong>!</p>';
+				}
 				echo '<p><a href="'.$urlHome.'depoimentos" class="button verde mini">+ depoimentos</a></p>';
 			echo '</div></div>';
 
 			$numDepo = '';
+			$taxDepo = '';
+			if (is_singular('product') && $ativa_LP == true) $taxDepo = 'template-aurora';
 			if (is_front_page()) { $numDepo = 8; } else { $numDepo = 5; }
-			afc_depoimentos($numDepo,'');
+			afc_depoimentos($numDepo,$taxDepo);
 		echo '</div>';
 
 
-		echo '<p data-aos="zoom-in" class="has-text-align-center cta"><a href="'.$urlHome.'contato" class="button rosa medio">Peça seu orçamento</a> <br> <a href="#faq" data-target="#faq" class="abre-modal"><small>Dúvidas? Veja o F.A.Q. do studio.</small></a></p>';
+		if (!is_singular('product')) echo '<p data-aos="zoom-in" class="has-text-align-center cta"><a href="'.$urlHome.'contato" class="button rosa medio">Peça seu orçamento</a> <br> <a href="#faq" data-target="#faq" class="abre-modal"><small>Dúvidas? Veja o F.A.Q. do studio.</small></a></p>';
 	echo '</section>';
 }
 
@@ -37,7 +46,7 @@ if (is_front_page() || is_post_type_archive(array('etheme_portfolio')) || is_pag
 // ========================================// 
 $temaurl = get_stylesheet_directory_uri();
 
-echo '<footer id="rodape"'.(is_front_page() || is_post_type_archive(array('etheme_portfolio','afc_depoimentos')) || is_page(array('servicos','sobre')) || is_singular('afc_blog') ? ' style="margin-top:0em;"' : '').'>';
+echo '<footer id="rodape"'.(is_front_page() || is_post_type_archive(array('etheme_portfolio','afc_depoimentos')) || (is_singular('product') && $ativa_LP == true) || is_page(array('servicos','sobre')) || is_singular('afc_blog') ? ' style="margin-top:0em;"' : '').'>';
 	echo '<nav class="menu-site" aria-label="Navegação do rodapé do site">';
 		echo '<ul id="navegacao">';
 			afc_menu('footer');
